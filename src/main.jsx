deea369e5cb8e1ad840e36ce5f96b60b266d22e6
i@aarsenev.ru <i@aarsenev.ru>
2025-05-31 01:10:08 +0300

    PWA приложение

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
-// src/main.jsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from './App';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        console.log('Есть новое обновление!');
+        // Можно тут сохранить состояние, чтобы показать кнопку обновления
+    },
+    onOfflineReady() {
+        console.log('Приложение готово к работе офлайн');
+    },
+});
+
+updateSW(); // запускаем проверку обновлений
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -12,3 +23,4 @@ ReactDOM.createRoot(rootElement).render(
         <AppRouter />
     </React.StrictMode>
 );
+
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,53 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     svgr({
-      exportAsDefault: false, // чтобы можно было писать { ReactComponent as ... }
-    })
+      exportAsDefault: false,
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'icons/icon-192x192.png', 'icons/icon-512x512.png'],
+      manifest: {
+        name: 'Моё PWA-приложение',
+        short_name: 'PWA App',
+        start_url: '/home',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/683a1ec37a078d0008e22c4f--sunny-bonbon-2effb7.netlify.app\/.*/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'runtime-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 86400,
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
   server: {
     allowedHosts: ['f156-89-23-103-229.ngrok-free.app']
