04ef0fb6908987f055696e1c32b492aeb3184162
i@aarsenev.ru <i@aarsenev.ru>
2025-06-05 16:11:50 +0300

    стартовые страницы

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import FinancePage from './pages/FinancePage';
